d2e9fc296bda0115b796a88cc9eb90b72e12e279
MiguelAngelTorres <torresma.1996@gmail.com>
2024-01-11 12:34:43 +0100

    Remove test code

--- prueba.py
+++ prueba.py
@@ -7,13 +7,7 @@ data = pd.read_csv("iris-species/Iris.csv")
 
 start = time.time()
 genetree = Genetree(data[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']], data[['Species']],
-                    score_function="auc", num_trees=100, deepness=4, num_rounds=5)
+                    score_function="auc", num_trees=100, deepness=4, num_rounds=10)
 end = time.time()
 print("\nMean time elapsed on warm: " + str(end-start))
 
-
-start = time.time()
-for i in range(1,30):
-    genetree.calculate_reproductivity_score()
-end = time.time()
-print("\nMean time elapsed on repro_score: " + str((end-start)/100))
